65c0b9d886dbc2b4cdc67ff07e3a53fe9537b275
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-09-17 04:41:41 +0530

    Added leaflet integration

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,21 @@
 const socket = io();
+const map = L.map("map").setView([0,0],16);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+const markers = {};
+
 if(navigator.geolocation){
     navigator.geolocation.watchPosition((position)=>{
         const {latitude,longitude} = position.coords;
         socket.emit("sendLocation",{latitude,longitude})
+        
+        if(markers[socket.id]){
+            markers[socket.id].setLatLng([latitude,longitude]);
+        } else {
+            markers[socket.id] = L.marker([latitude,longitude]).addTo(map);
+        }
+        
+        map.setView([latitude,longitude],16);
     }, (error)=>{
         console.error(error);
     },
@@ -13,3 +26,20 @@ if(navigator.geolocation){
     }
 )
 }
+
+socket.on("receiveLocation",(data)=>{
+    const {id,latitude,longitude} = data;
+    
+    if(markers[id]){
+        markers[id].setLatLng([latitude,longitude]);
+    } else{
+        markers[id] = L.marker([latitude,longitude]).addTo(map);
+    }
+})
+
+socket.on("userDisconnected",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
--- script.js
+++ script.js
@@ -11,18 +11,47 @@ const __dirname = path.dirname(__filename);
 const server =http.createServer(app);
 const io = new Server(server);
 
+let userCount = 0;
+const connectedUsers = new Map();
 
 io.on("connection",(socket)=>{
-    console.log("a User connected ")
+    userCount++;
+    const isFirstUser = userCount === 1;
+    
+    console.log(`User ${socket.id} connected. Total: ${userCount}, First: ${isFirstUser}`);
+    
+    // Send all existing user locations to the new connection
+    connectedUsers.forEach((location, userId) => {
+        if(userId !== socket.id) {
+            socket.emit("receiveLocation", {id: userId, ...location});
+        }
+    });
+    
+    socket.on("checkFirstUser", () => {
+        socket.emit("userStatus", {isFirst: isFirstUser});
+        console.log(`Sent user status to ${socket.id}: First=${isFirstUser}`);
+    });
+    
+    socket.on("sendLocation",function(data){
+        console.log(`Location from ${socket.id}:`, data);
+        connectedUsers.set(socket.id, data);
+        io.emit("receiveLocation",{id:socket.id, ...data});
+    })
+    
+    socket.on("disconnect",()=>{
+        userCount--;
+        connectedUsers.delete(socket.id);
+        console.log(`User ${socket.id} disconnected. Total: ${userCount}`);
+        io.emit("userDisconnected",socket.id);
+    })
 })
+
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=>{
     res.render("index")
 })
-
-
 
 server.listen(3000,()=>{
     console.log("Server is running on port 3000")
